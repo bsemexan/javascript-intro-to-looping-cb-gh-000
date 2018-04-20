5ce7bd2b13c150e5eb86d6f1825f7eb0201e742b
Berry  Semexan <bsemexan@gmail.com>
2018-04-20 21:31:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@ function forLoop(array){
   var arr = [];
   for(let i = 0; i < 25; i++){
     if (i === 1){
-      arr += 'I am 1 strange loop';
+      arr += 'I am 1 strange loop.';
     } else {
-      arr += 'I am ${i} strange loops';
+      arr += 'I am + +'strange loops.';
     }
   }
   return arr
